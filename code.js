5a5c7aeee7da908fe5a28e8c0a5ca6e7ceaf3acd
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-02-12 16:10:40 -0700

    Code done

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function fib(n) {
-    return n;
+    var arr = new Array(n)
+
+    function findFib(Array, index){
+       if (index == 0){
+        Array[index] = 0
+        return 
+       }
+       else if (index == 1){
+        Array[index-1] = 0
+        Array[index] = 1
+        return 
+       }
+       else{
+        findFib(Array, index-1) 
+        Array[index] = Array[index-2] + Array[index-1]
+        return 
+       }
+    }
+    
+    findFib(arr, n);
+    return arr
 }
