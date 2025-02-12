7b9167e837c3c96c705fdd75b6ee4064a3e92960
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-02-12 16:18:03 -0700

    how about now?

--- code.js
+++ code.js
@@ -3,22 +3,22 @@ function fib(n) {
     //use the numberprovided as an index into the array
     //if the index is either of the base cases just set the index to the base cases
     //else add the two last indexes together to create a third index, repeat untill you get to the top of the recursion
-    var arr = new Array(n)
+    var arr = new Array(n);
 
     function findFib(Array, index){
        if (index == 0){
-        Array[index] = 0 
+        Array[index] = 0;
        }
        else if (index == 1){
-        Array[index-1] = 0
-        Array[index] = 1
+        Array[index-1] = 0;
+        Array[index] = 1;
        }
        else{
-        findFib(Array, index-1) 
-        Array[index] = Array[index-2] + Array[index-1]
+        findFib(Array, index-1) ;
+        Array[index] = Array[index-2] + Array[index-1];
        }
     }
     
     findFib(arr, n);
-    return arr
+    return arr;
 }
